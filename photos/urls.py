b4ac2da5d4dd19ddfdef826acd741926c892e9aa
NancyWachiuri <nancywachiuri@gmail.com>
2021-10-15 11:01:01 +0300

    Add search url

--- photos/urls.py
+++ photos/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('', views.gallery, name = 'gallery'),
     path ('photo/<str:pk>/', views.viewPhoto, name='photo'),
     path('add/',views.addPhoto, name='add'),
+    path(r'^search/', views.search_results, name='search_results')
 ]
 
 # if settings.DEBUG:
