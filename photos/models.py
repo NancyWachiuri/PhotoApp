9a476b8dec3b70fd5b493cacc12369381ccdb373
NancyWachiuri <nancywachiuri@gmail.com>
2021-10-15 11:00:32 +0300

    Add search module

--- photos/models.py
+++ photos/models.py
@@ -4,6 +4,11 @@ from django.db import models
 
 class Category(models.Model):
     name= models.CharField(max_length=100, null=False, blank=False)
+    @classmethod
+    
+    def search_by_title(cls,category_name):
+        photo = cls.objects.filter(title__icontains=category_name)
+        return photo
 
     def __str__(self):
         return self.name
