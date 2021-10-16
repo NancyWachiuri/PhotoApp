b24513c16aec04e27764f4cc32a00ed76fa820f9
NancyWachiuri <nancywachiuri@gmail.com>
2021-10-16 13:39:44 +0300

    Add location html url

--- photos/urls.py
+++ photos/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('', views.gallery, name = 'gallery'),
     path ('photo/<str:pk>/', views.viewPhoto, name='photo'),
     path('add/',views.addPhoto, name='add'),
-    path(r'^search/', views.search_results, name='search_results')
+    path('search/', views.search_results, name='search_results'),
+    path('location/<int:location_id>/', views.location, name='location'),
 ]
 
 # if settings.DEBUG:
